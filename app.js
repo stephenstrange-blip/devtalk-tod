08ac0c4f311a57d003f6c0dce848b70fd5c99bc7
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-05-10 17:40:59 +0800

    Fix: Change app.get to app.use

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const path = require("node:path");
 const express = require("express");
 const { argv } = require("node:process");
-const { indexRouter } = require("./routes/route");
+const indexRouter = require("./routes/route");
 
 require("dotenv").config();
 
@@ -12,7 +12,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", indexRouter);
+app.use("/", indexRouter);
 
 app.listen(8080, "localhost", () => {
   console.log(`Server listening at http://localhost:8080`);
