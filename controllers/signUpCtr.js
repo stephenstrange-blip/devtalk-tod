90e8ff9e3fff8ea0e24e54303538f5df315d0d0a
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-05-11 07:56:46 +0800

    Feat: Hash password input

--- controllers/signUpCtr.js
+++ controllers/signUpCtr.js
@@ -1,24 +1,26 @@
 const { addUser } = require("../db/database");
+const bcrypt = require("bcrypt");
 
 exports.get = async (req, res) => {
   res.status(200).render("sign-up");
 };
 
 exports.post = async (req, res) => {
-  console.log(req.body);
   const { firstName, lastName, userName, password } = req.body;
+
+  const hashed = await bcrypt.hash(password, 5);
 
   try {
     const result = await addUser({
       firstName,
       lastName,
       username: userName,
-      password,
+      password: hashed,
     });
     console.log(result);
     res.status(200).redirect("/");
   } catch (err) {
-    console.log(error);
+    console.log(err);
     res.status(400).redirect("/");
   }
 };
