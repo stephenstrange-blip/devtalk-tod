65c9af0b26e9d1523438f58b58bb066a14e95302
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-05-11 19:13:40 +0800

    Build: Add a welcome page and route

---
+++ controllers/welcomeCtr.js
@@ -0,0 +1,3 @@
+exports.get = async (req, res) => {
+  res.status(200).render("welcome");
+};
--- routes/route.js
+++ routes/route.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const indexController = require("../controllers/indexCtr").controller;
 const signUpController = require("../controllers/signUpCtr");
+const welcomeController = require("../controllers/welcomeCtr");
 const passport = require("passport");
 const LocalStrategy = require("../db/passport").LocalStrategy;
 
@@ -10,13 +11,15 @@ indexRouter.get("/", indexController);
 indexRouter.get("/sign-up", signUpController.get);
 indexRouter.post("/sign-up", signUpController.post);
 
+indexRouter.get("/welcome", welcomeController.get);
+
 indexRouter.get("/login", (req, res) => res.render("log-in"));
 indexRouter.post(
   "/login",
   passport.authenticate("local", {
     successRedirect: "/welcome",
     successMessage: true,
-    failureRedirect: "/",
+    failureRedirect: "/login",
     failureMessage: true,
   })
 );
