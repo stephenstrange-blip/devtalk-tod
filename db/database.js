15dfcf0b04a519237fb28e06216c3800cfd5adab
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-05-13 22:17:14 +0800

    Build: Write functions to add and log a new message, and fetch all message

--- db/database.js
+++ db/database.js
@@ -38,5 +38,36 @@ const updateRole = async (user_id, role_id) => {
   return result;
 };
 
-module.exports = { addUser, getPassCode, updateRole };
+const addMessage = async (title, message) => {
+  const result = await pool.query(
+    "INSERT INTO messages (title, message) VALUES ($1, $2) RETURNING id",
+    [title, message]
+  );
+  return result;
+};
+
+const getMessages = async () => {
+  const result = await pool.query(
+    " SELECT u.firstName, u.lastName, m.message, m.title, to_char(l.created, 'Day, DD, Month, YYYY') AS date FROM users AS u INNER JOIN log AS l ON u.id = l.user_id INNER JOIN messages AS m ON l.message_id = m.id;"
+  );
+  console.log(result);
+  return result;
+};
+
+const logMessage = async (msg_id, user_id) => {
+  const result = await pool.query(
+    "INSERT INTO log (message_id, user_id) VALUES ($1, $2)",
+    [msg_id, user_id]
+  );
+  return result;
+};
+
+module.exports = {
+  addUser,
+  getPassCode,
+  updateRole,
+  addMessage,
+  logMessage,
+  getMessages,
+};
 module.exports.pool = pool;
