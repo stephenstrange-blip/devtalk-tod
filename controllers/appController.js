d5276c727df36e8d1d109f0d6102301cea993016
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-05-13 20:49:29 +0800

    Chore: Redirect to welcome page if a user is logged in

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,6 +1,8 @@
 exports.middleware = async (req, res, next) => {
   const sessionMsg = req.session.messages;
 
+  console.log(req.url);
+
   if (sessionMsg && sessionMsg.length > 1) {
     req.session.messages.shift();
   }
--- controllers/indexCtr.js
+++ controllers/indexCtr.js
@@ -1,4 +1,5 @@
 exports.controller = async (req, res) => {
   if (req.session.messages) console.log(req.session.messages);
+  if (res.locals.currentUser) return redirect("/welcome");
   res.status(200).render("index");
 };
