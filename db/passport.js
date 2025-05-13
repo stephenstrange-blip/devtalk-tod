6dd6ee44f7f45ca854ea6c5e298a852e0a9f3fe5
jade <121040221+stephenstrange-blip@users.noreply.github.com>
2025-05-13 12:23:15 +0800

    Chore: Input correct error message

--- db/passport.js
+++ db/passport.js
@@ -25,7 +25,7 @@ passport.use(
 
       const match = await bcrypt.compare(password, user.password);
       if (!match) {
-        return done(null, false, { message: "Correct password" });
+        return done(null, false, { message: "Incorrect password" });
       }
 
       return done(null, user);
